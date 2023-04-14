a28ed9bdd216715d70d614033bb4e3ca723307d1
harakki <124341753+harakki@users.noreply.github.com>
2023-04-14 10:05:22 +0700

    Make formatting using .CLANG-FORMAT

--- src/libgeometry/calculate_circle.c
+++ src/libgeometry/calculate_circle.c
@@ -1,7 +1,8 @@
 #include "calculate_circle.h"
+
 #define pi 3.1415926535
-void  calculate_circle(double r, double* area_num, double* perimetr)
-{
+
+void calculate_circle(double r, double* area_num, double* perimetr) {
     *area_num = pi * r * r;
     *perimetr = 2 * pi * r;
 }
--- src/libgeometry/calculate_circle.h
+++ src/libgeometry/calculate_circle.h
@@ -1,2 +1,3 @@
-#pragma once
-void calculate_circle();
+	#pragma once
+	
+	void calculate_circle();
